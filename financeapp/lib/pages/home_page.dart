ec54b7f334fc1549cad29d37dc56040b576fcdbb
Hiền Ca <nguyenvanhien050601@gmail.com>
2023-07-31 10:52:07 +0700

    lastest

---
+++ financeapp/lib/pages/home_page.dart
@@ -0,0 +1,90 @@
+import 'package:animated_bottom_navigation_bar/animated_bottom_navigation_bar.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:financeapp/pages/daily_page.dart';
+import 'package:financeapp/pages/transection_page.dart';
+import 'package:financeapp/theme/colors.dart';
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  int pageIndex = 0;
+
+  List<Widget> pages = [
+    const DailyPage(),
+    const TransectionPage(),
+    const TransectionPage(),
+    const TransectionPage(),
+    const TransectionPage(),
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: primary,
+      body: getBody(),
+      bottomNavigationBar: getFooter(),
+      floatingActionButton: SafeArea(
+        child: SizedBox(
+          // height: 30,
+          // width: 40,
+          child: FloatingActionButton(
+            onPressed: () {},
+            backgroundColor: buttoncolor,
+            child: const Icon(
+              Icons.add,
+              size: 20,
+            ),
+            // shape:
+            //     BeveledRectangleBorder(borderRadius: BorderRadius.circular(4)),
+          ),
+        ),
+      ),
+      floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+    );
+  }
+
+  Widget getBody() {
+    return IndexedStack(
+      index: pageIndex,
+      children: pages,
+    );
+  }
+
+  Widget getFooter() {
+    List<IconData> iconItems = [
+      CupertinoIcons.home,
+      CupertinoIcons.creditcard,
+      CupertinoIcons.money_dollar,
+      CupertinoIcons.person,
+    ];
+    return AnimatedBottomNavigationBar(
+        backgroundColor: primary,
+        icons: iconItems,
+        splashColor: secondary,
+        inactiveColor: black.withOpacity(0.5),
+        gapLocation: GapLocation.center,
+        activeIndex: pageIndex,
+        notchSmoothness: NotchSmoothness.softEdge,
+        leftCornerRadius: 10,
+        iconSize: 25,
+        rightCornerRadius: 10,
+        elevation: 2,
+        onTap: (index) {
+          setTabs(index);
+        });
+  }
+
+  setTabs(index) {
+    setState(() {
+      pageIndex = index;
+      // ignore: avoid_print
+      print("page: $index");
+    });
+  }
+}
